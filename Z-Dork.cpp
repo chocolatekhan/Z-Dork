445d56a5f186172477c048aab302e28341fd418e
Alvi Khan <55395950+chocolatekhan@users.noreply.github.com>
2019-09-30 02:20:38 +0600

    Improved gameplay algorithm

--- Z-Dork.cpp
+++ Z-Dork.cpp
@@ -2,19 +2,130 @@
 #include<stdlib.h>
 #include"customgraphics.h"
 
-//parts will need to be rearranged based on when things are used.
-
-struct stats             //statistics about characters
+//statistics about characters
+struct stats
 {
     int health;
     int heal_potion;
     char name[100];
+    int score;
 };
 
-struct stats player1 = {100, 5, "Player 1"};
-struct stats player2 = {100, 5, "Player 2"};
+struct stats player1 = {100, 5, "Player 1", 0};
+struct stats player2 = {100, 5, "Player 2", 0};
 
-int heal(struct stats *character)                      //heal function
+
+//declaration of all user-defined functions
+int intro();
+void player1_action();
+void player2_action();
+int heal (struct stats *character);
+void attack (struct stats *character1, struct stats *character2);
+void damage (struct stats *character);
+int win(struct stats *character);
+int reset(struct stats *character1, struct stats *character2);
+void endgame();
+
+
+int main()
+{
+    printf("Welcome to Z-Dork.\n\n");
+    int gdriver = DETECT, gmode;
+    initgraph (&gdriver, &gmode, NULL);
+    initwindow(1280, 960);                  //graphics window size
+    //readimagefile("Logo.jpg", 0,80, 1280, 720);
+    //delay(2000);
+    //cleardevice();
+    player_names();
+    health_bar1(100);
+    //heal_potion1(5);
+    health_bar2(100);
+    intro();
+}
+
+
+int intro()
+{
+    printf("\n\nPlayer 1 Heal Potions:%d\n\nPlayer 2 Heal Potions: %d\n\n", player1.heal_potion, player2.heal_potion);
+    //line above temporary
+    player1_action();
+}
+
+void player1_action()
+{
+    int action = 0;
+    while ((action != 1) && (action != 2))
+    {
+        text_left("Player 1's Turn");
+        text_right(" ");
+        printf("Choose an action:\n\t1. Attack\n\t2. Heal\nAction: ");
+        scanf("%d", &action);
+        if (action == 1)        attack(&player1, &player2);
+        else if (action == 2)
+        {
+            if (heal(&player1) == 0)    player1_action();
+        }
+        else
+        {
+            text_left("Invalid choice.\n");
+            delay(1000);
+        }
+    }
+
+    if (player2.health <= 0)    win(&player1);
+    else                        player2_action();
+}
+
+void player2_action()
+{
+    int action = 0;
+    while ((action != 1) && (action != 2))
+    {
+        text_right("Player 2's Turn");
+        text_left(" ");
+
+        printf("Choose an action:\n\t1. Attack\n\t2. Heal\nAction: ");
+        scanf("%d", &action);
+        if (action == 1)        attack(&player2, &player1);
+        else if (action == 2)
+        {
+            if (heal(&player2) == 0)    player2_action();
+        }
+        else
+        {
+            text_right("Invalid choice.\n");
+            delay(1000);
+        }
+    }
+
+    if (player1.health <= 0)    win(&player2);
+    else                        intro();
+}
+
+void attack (struct stats *character1, struct stats *character2)
+{
+    int char1_attack, char2_dodge;
+    printf("\n\nChoose an attack:\n1. Attack high\n2. Attack low\nChoice: ");
+    scanf("%d", &char1_attack);
+    printf("\n\n%s has attacked! Choose a dodge:\n1. Dodge high\n2. Dodge low\nChoice: ", character1->name);
+    scanf("%d", &char2_dodge);
+    if (char1_attack == char2_dodge)
+    {
+        damage(character2);
+        printf("\n%s failed to dodge the attack. Damage 10.\n", character2->name);
+    }
+    else    printf("%s has dodge the attack.\n", character2->name);
+
+}
+
+void damage(struct stats *character)
+{
+    character->health -= 10;
+    if (strcmp(character->name, "Player 1") == 0)   health_bar1(character->health);
+    else                                            health_bar2(character->health);
+}
+
+int heal(struct stats *character)
 {
     if (character->health == 100)
     {
@@ -37,64 +148,27 @@ int heal(struct stats *character)                      //heal function
     }
 }
 
-void attack(struct stats *character1, struct stats *character2)              //attack function
+
+int win(struct stats *character)
 {
-    if (rand()%2 == 0)
+    char ans;
+    character->score++;
+    printf("\n\nCongratulations. %s has won.\n\n", character->name);
+
+    next_round:
+    printf("Play another round? (Y/N)\n\n");
+    scanf("%c", &ans);
+    if (ans == 'Y' || ans == 'y')
     {
-        int initial_health = character2->health;
-        character2->health -= 10;
-        printf("\n\nYou caused %d damage.\n\n", initial_health-character2->health);
-        if (strcmp(character2->name, "Player 1") == 0)  health_bar1(character2->health);
-        else                                            health_bar2(character2->health);
+        reset(&player1, &player2);
+        intro();
     }
+    else if (ans == 'N' || ans == 'n')  endgame();
     else
     {
-        printf("\n\nYour attack failed.\n\n");
-        return;
+        printf("Invalid response.\n\n");
+        goto next_round;
     }
-
-}
-
-int intro();
-int win(struct stats *character);
-void player2_action();
-
-void player1_action()
-{
-    int action = 0;
-    while ((action != 1) && (action != 2))
-    {
-        printf("Player 1's Turn.\n\nChoose an action:\n\t1. Attack\n\t2. Heal\nAction: ");
-        scanf("%d", &action);
-        if (action == 1)        attack(&player1, &player2);
-        else if (action == 2)
-        {
-            if (heal(&player1) == 0)    player1_action();
-        }
-        else                    printf("Invalid choice.\n");
-    }
-
-    if (player2.health <= 0) win(&player1);
-    else                    player2_action();
-}
-
-void player2_action()
-{
-    int action = 0;
-    while ((action != 1) && (action != 2))
-    {
-        printf("Player 2's Turn.\n\nChoose an action:\n\t1. Attack\n\t2. Heal\nAction: ");
-        scanf("%d", &action);
-        if (action == 1)        attack(&player2, &player1);
-        else if (action == 2)
-        {
-            if (heal(&player2) == 0)    player2_action();
-        }
-        else                    printf("Invalid choice.\n");
-    }
-
-    if (player1.health <= 0) win(&player2);
-    else                    intro();
 }
 
 int reset(struct stats *character1, struct stats *character2)
@@ -107,46 +181,7 @@ int reset(struct stats *character1, struct stats *character2)
     health_bar2(100);
 }
 
-
-int win(struct stats *character)
+void endgame()
 {
-    char ans;
-    printf("\nCongratulations. %s has won.\n\n", character->name);
 
-    next_round:
-    printf("Play another round? (Y/N)\n\n");
-    scanf("%c", &ans);
-    if (ans == 'Y' || ans == 'y')
-    {
-        reset(&player1, &player2);
-        intro();
-    }
-    else if (ans == 'N' || ans == 'n')  exit(1);
-    else
-    {
-        printf("Invalid response.\n\n");
-        goto next_round;
-    }
-}
-
-int intro()
-{
-    printf("\n\nPlayer 1 Statistics:\nHealth: %d\nHeal Potions:%d\n\nPlayer 2 Statistics:\nHealth: %d\nHeal Potions: %d\n\n", player1.health, player1.heal_potion, player2.health, player2.heal_potion);
-    //line above temporary
-    player1_action();
-}
-
-int main()
-{
-    printf("Welcome to Z-Dork.\n\n");
-    int gdriver = DETECT, gmode;
-    initgraph (&gdriver, &gmode, NULL);
-    initwindow(1280, 960);                  //graphics window size
-    readimagefile("Logo.jpg", 0,80, 1280, 720);
-    delay(2000);
-    cleardevice();
-    player_names();
-    health_bar1(100);
-    health_bar2(100);
-    intro();
 }
